9e4d60d19ea64f8325e760f42e1c17ee08fd41c3
BoomBoom <nomenator@hotmail.com>
2021-01-06 19:07:44 -0500

    Basic blazor application with a single custom component.

---
+++ src/BlazorDataBinding/BindAttributes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorDataBinding
+{
+    [BindElement("ValueBindingComponent", null, "value", "ValueChangedEvent")]
+    public static class BindAttributes
+    {
+    }
+}
